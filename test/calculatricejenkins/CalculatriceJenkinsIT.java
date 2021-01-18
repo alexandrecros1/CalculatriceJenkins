1c63380212103885dac6aa7e174f9db7dc2cc1f2
alexandrecros1 <46493132+alexandrecros1@users.noreply.github.com>
2021-01-18 16:00:14 +0100

    calculatrice implementee

---
+++ test/calculatricejenkins/CalculatriceJenkinsIT.java
@@ -0,0 +1,39 @@
+package calculatricejenkins;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatriceJenkinsIT {
+    
+    public CalculatriceJenkinsIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        CalculatriceJenkins.main(args);
+        fail("The test case is a prototype.");
+    }
+    
+}
